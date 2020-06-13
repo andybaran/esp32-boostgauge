e0e72fd129761f411443784cc45b09b15c494b88
Andy Baran <andy.baran@hashicorp.com>
2020-06-13 07:31:10 -0400

    whatever

--- boost/boost.ino
+++ boost/boost.ino
@@ -7,10 +7,7 @@
  * TODO : Figure out curve for temperature sensor and log air temp
  */
 
-#include <Wire.h>
 #include "SparkFunBME280.h"
-
-#include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
@@ -18,31 +15,29 @@
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 #define OLED_RESET -1
 
-// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT);
 float atmpressure;
 BME280 mySensor;
+float out_voltage;
+float Pabs;
+int mapsen = A0; // Set MAP sensor input on Analog port 0 
+unsigned long previousMillis = 0;
+const long interval = 250;
+int min = 0;
+int max = 0;
 
-// Bosch MAP sensor setup
-int mapsen = 26; // Set MAP sensor input on Analog port 0 
-
-// float value and value in bytes
 typedef union {
   int psiPabsInt;
   byte psiPabsByte[4];
 } psiPabs;
 
 psiPabs psiDifference;
-//float psiPabs; // for converting our value in mbar to psi
-
-//non blocking delay using millis from https://randomnerdtutorials.com/why-you-shouldnt-always-use-the-arduino-delay-function/
-unsigned long previousMillis = 0;
-const long interval = 250;
 
 
 void setup() { 
-  Serial1.begin(115200, SERIAL_8N1, 16, 17);
+  Serial1.begin(115200);//, SERIAL_8N1, 16, 17);
+  Serial.begin(115200);
   Wire.begin();
 
   if (mySensor.beginI2C() == false) //Begin communication over I2C
@@ -54,7 +49,6 @@ void setup() {
   //Use "Game mode" according to honeyewll datasheet
   mySensor.setFilter(4);
   mySensor.setPressureOverSample(3);
-  atmpressure = mySensor.readFloatPressure() * 0.0001450377;
 
   if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) { // Address 0x3D for 128x64
     Serial.println(F("SSD1306 allocation failed"));
@@ -66,59 +60,49 @@ void setup() {
     
 void loop() { 
 
+  //non blocking delay using millis from https://randomnerdtutorials.com/why-you-shouldnt-always-use-the-arduino-delay-function/
   unsigned long currentMillis = millis();
   if (currentMillis - previousMillis >= interval) {
       previousMillis = currentMillis;
-      float out_voltage = analogRead(mapsen) / 1000.000000; //esp32 is reading in millivolts so let's convert to volts for our curve to work
+      atmpressure = mySensor.readFloatPressure() * 0.0001450377;
+      out_voltage = analogRead(mapsen) * (5.0/1023);
   
-      // forumula derived from https://www.youtube.com/watch?v=83LuzJTIbAw&list=PLZ73CAxmn6f3F2Muguy2oU4sQEykYiTsw&index=5&t=375s
-      float Pabs = 3294.11764705*(out_voltage/4.68)-279.99999999925; 
-      /*4.764 from laptop
-       * 4.679 - 4.8 from car stereo usb out
-       * 
-      */
+      Pabs =  311.1111111 * (out_voltage/4.97) - 1.333333333;
   
-      // pressure in PSI from Bosch MAP minus BME280 + .5 as that seemed to be a consistent difference
-      Pabs = (Pabs / 68.94) - atmpressure; // Convert mbar to PSI and subtract ambient so we can calculate vacuum as well
+      // pressure in PSI from Bosch MAP minus BME280 + .5 as that seemed to be a consistent difference, trusting Bosch as more accurate
+      Pabs = round((Pabs / 6.894)) - round(atmpressure +.5); // Convert kPa to PSI and subtract ambient so we can calculate vacuum as well
       
-        display.clearDisplay();
-        display.setTextSize(2);
-        display.setTextColor(WHITE);
-        display.setCursor(0, 0);
+      display.clearDisplay();
+      display.setTextSize(2);
+      display.setTextColor(WHITE);
+      display.setCursor(0, 0);
       
       //if value is less than atmostpheric pressure convert to inhg and display vacuum
       if (Pabs < 0) {
         Pabs = -1*(Pabs*2.036020657601236); //convert to inhg
         psiDifference.psiPabsInt = round(Pabs);
-       /* Serial.println(inHG);
-        Serial.println(Pabs);
-        Serial.println(atmpressure);*/
         display.print(psiDifference.psiPabsInt);
-        display.print(" inHG.");
-        display.display(); 
-      }
-      else {
-        psiDifference.psiPabsInt = round(Pabs);
-        /*Serial.println(psi);
-        Serial.println(Pabs);
-        Serial.println(atmpressure);*/
-        display.print(psiDifference.psiPabsInt);
-        display.print(" PSI.");
-        display.display(); 
+        display.print(" inHG\n");
       }
       
-      // Uncommment lines 80 - 82 and comment line 84 to use for testing when car is unavailable 
-      /*long randomNum = random(-20,20);
-      int randomInt = (int)randomNum;
-      psiDifference.psiPabsInt = round(Pabs) + randomInt*///floor(psiDifference.psiPabsFloat*10+0.5)/10; //round to nearest tenth
+      else {
+        psiDifference.psiPabsInt = round(Pabs);
+        display.print(psiDifference.psiPabsInt);
+        display.print(" PSI\n");
+      }
 
-      if (Serial1.availableForWrite() > 0) {
-        Serial1.write(psiDifference.psiPabsByte,4);
+      //update max and min
+      if (psiDifference.psiPabsInt < max) {max = psiDifference.psiPabsInt;}
+      if (psiDifference.psiPabsInt > min) {min = psiDifference.psiPabsInt;}
+      
+      display.print(String("\nmax: ") + String(max));
+      display.print(String("\nmin: ") + String(min));
+      display.display(); 
+
+      if (Serial.availableForWrite() > 0) {
+        Serial.write(psiDifference.psiPabsByte,4);
       } else {
         Serial.println("The buffers are full");
       }
-      
-
-
   }
 }
